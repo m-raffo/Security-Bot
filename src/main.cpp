1ca622ccc19823296d5e429efaf003be9cad2b48
Mason Raffo <mason@dhcp-clara-3945.eduroam.cornell.edu>
2019-07-07 10:38:49 -0400

    Read and test ultrasonic sensor

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,18 @@
 #include <Arduino.h>
 #include <QTRSensors.h>
 #include "FastLED.h"
+#include "NewPing.h"
 
 // Define led constants
 #define NEOPIXEL_PIN 1
 #define NUM_LEDS 7
 
+// Define ultrasonic sensor constants
+#define TRIGGER_PIN  26
+#define ECHO_PIN     13
+#define MAX_DISTANCE 200
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 // Define QTR sensor variables
 QTRSensors qtr;
@@ -52,6 +59,12 @@ void setup() {
     fill_solid(leds, NUM_LEDS, CRGB::Black);
     FastLED.show();
 
+    // Give a little time to see the test results
+    delay(750);
+
+    // Erase test output message
+    M5.Lcd.clear(BLACK);
+
   } else {
     // Play fail sound here
 
@@ -76,25 +89,42 @@ void loop() {
   // Read line sensor values
   qtr.read(lineSensorValues);
 
+  // Display sonar pings
+  // M5.Lcd.clear(BLACK);
+  M5.Lcd.setCursor(10, 10);
+  M5.Lcd.printf("%03d", sonar.ping_cm());
+
+  delay(50);
+
+
 }
 
-// NOTE: All functions of the robot must be initialized before this function is called
+/**
+ * Checks all systems of the robot and prints the test results to the screen of
+ * the robot.
+ *
+ * NOTE: All functions of the robot must be initialized before calling this function
+ * This function checks the following functions:
+ *   QTR Sensor
+ *   Ultrasonic Sensor
+ *
+ * Sample output error message:
+
+     Sensor tests:
+     QTR - [SUCCESS] - (60, 49, 54)
+     Ultrasonic - [FAIL] - (0.0000000000)
+
+ * @return true if all checks good, false if not
+ */
 boolean performChecks() {
-  /*
-    This function checks the following functions:
-      QTR Sensor
-
-    Sample output error message:
-
-    Sensor tests:
-    QTR - [SUCCESS] - (60, 49, 54)
-  */
-
   // Define test variables
   boolean allGood = true;
 
   boolean qtrStatus = true;
   uint16_t qtrValues[qtrSensorCount];
+
+  boolean sonicStatus = true;
+  int sonicValue;
 
   // Get line sensor values
   qtr.read(qtrValues);
@@ -105,8 +135,19 @@ boolean performChecks() {
     qtrStatus = false;
   }
 
+  // Ultrasonic sensor tests
+  sonicValue = sonar.ping_cm();
+  if(sonicValue == 0) {
+    allGood = false;
+    sonicStatus = false;
+  }
+
+
   // Display test results
   M5.Lcd.println("Sensor tests:");
+
+
+  // Print line sensor results
   M5.Lcd.print("QTR - [");
   if(qtrStatus) {
     M5.Lcd.setTextColor(GREEN, BLACK);
@@ -119,6 +160,20 @@ boolean performChecks() {
 
   M5.Lcd.setTextColor(WHITE, BLACK);
   M5.Lcd.printf("] - (%d, %d, %d)\n", qtrValues[0], qtrValues[1], qtrValues[2]);
+
+  // Print ultrasonic results
+  M5.Lcd.print("Ultrasonic - [");
+  if(sonicStatus) {
+    M5.Lcd.setTextColor(GREEN, BLACK);
+    M5.Lcd.print("SUCCESS");
+
+  } else {
+    M5.Lcd.setTextColor(RED, BLACK);
+    M5.Lcd.print("FAIL");
+  }
+
+  M5.Lcd.setTextColor(WHITE, BLACK);
+  M5.Lcd.printf("] - (%d)\n", sonicValue);
 
 
   return allGood;
